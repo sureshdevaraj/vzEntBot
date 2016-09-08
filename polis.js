53902f2c9ea1f4ba283f32fa66b34cb33042a6ac
Administrator <Administrator@SCSBWIN-667643>
2016-09-08 01:05:23 -0700

    return ({         speech: "Here are some recommendations for tonight",         displayText: "TV recommendations",         data: aa,         source: "Zero Service - app_zero.js"     });

--- polis.js
+++ polis.js
@@ -176,17 +176,17 @@ function recommendTVNew1(apiresp) {
     //var output = eval('(' + JSON.stringify(apiresp) + ')');
     console.log("apiresp1:" + JSON.stringify(objToJson));
     //console.log("output1:" + output);
-    //var aa = JSON.parse(output);
+    var aa = JSON.parse(apiresp);
     //console.log(aa);
-    return objToJson;
-    /*
+    //return objToJson;
+    
     return ({
         speech: "Here are some recommendations for tonight",
         displayText: "TV recommendations",
-        data: output,
+        data: aa,
         source: "Zero Service - app_zero.js"
     });
-    */
+    
 }
 function recommendTV() {
     return ({
