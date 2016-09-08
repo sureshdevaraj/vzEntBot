d2ae9b9fcdf0aa9ff4e07caade98bcbcc0e8a778
Administrator <Administrator@SCSBWIN-667643>
2016-09-08 02:07:18 -0700

    data: parsedResponse["items"][0]["data"]["facebook"],

--- polis.js
+++ polis.js
@@ -176,16 +176,16 @@ function recommendTVNew1(apiresp) {
     //var output = eval('(' + JSON.stringify(apiresp) + ')');
     console.log("apiresp1:" + JSON.stringify(objToJson));
     //console.log("output1:" + output);
-    var aa = JSON.parse(apiresp);
-    var cc = aa.data.facebook.objToJson;
-    console.log("objToJson"+cc);
+    var parsedResponse = JSON.parse(apiresp);
+    
+    
     //console.log(aa);
     //return objToJson;
     
     return ({
         speech: "Here are some recommendations for tonight",
         displayText: "TV recommendations",
-        data: aa.data.facebook.objToJson,
+        data: parsedResponse["items"][0]["data"]["facebook"],
         source: "Zero Service - app_zero.js"
     });
     
