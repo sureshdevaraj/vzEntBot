b9e60d209f39873d13a4f0d800f521bd988d890f
sureshdevaraj <suresh.k.devaraj@gmail.com>
2016-09-20 09:53:24 +0530

    .

--- polis.js
+++ polis.js
@@ -33,11 +33,32 @@ router.post('/webhook', function (req, res) {
      
 });
 
+router.get('/webhook', function (req, res) {
+
+    var headersInfo = { "Content-Type": "application/json" };
+    var Client = require('node-rest-client').Client;
+    var client = new Client();
+    var args = {
+        "headers": headersInfo
+    };
+
+    var req = client.post("http://localhost:58328/Handler1.ashx", args, function (data, response) {
+
+        var parsedData = "";
+        console.log("success1");
+        parsedData = JSON.parse(data);
+        res.json(parsedData);
+
+    });
+
+
+});
+
 // more routes for our API will happen here
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 
 app.use('/api', router);
-app.listen(PORT, function () {
+app.listen(3000, function () {
     console.log('Listening on port ' + PORT) ;
 });
