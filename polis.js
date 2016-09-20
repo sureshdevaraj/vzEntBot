60e5554a3126f983369a2ce9a4a16f531bbf5957
sureshdevaraj <suresh.k.devaraj@gmail.com>
2016-09-20 10:25:48 +0530

    https://vzentpoljs.herokuapp.com/api/vzwhatshot

--- polis.js
+++ polis.js
@@ -42,7 +42,7 @@ router.get('/webhook', function (req, res) {
         "headers": headersInfo
     };
 
-    var req = client.post("https://www98.verizon.com/foryourhome/vzrepair/flowengine/vzwhatshot.ashx", args, function (data, response) {
+    var req = client.post("https://vzentpoljs.herokuapp.com/api/vzwhatshot", args, function (data, response) {
 
         var parsedData = "";
         console.log("success1");
@@ -53,6 +53,15 @@ router.get('/webhook', function (req, res) {
 
 
 });
+
+router.get('/vzwhatshot', function (req, res) {
+    var yourJson = "'facebook': { 'attachment': { 'type': 'template','payload': { 'template_type': 'generic','elements': [{'title': 'Shark Tank','subtitle': 'Shark Tank','image_url': 'http://image.vam.synacor.com.edgesuite.net/0f/07/0f07592094a2a596d2f6646271e9cb0311508415/w=414,h=303,crop=auto/?sig=88c390c980d4fa53d37ef16fbdc53ec3dfbad7d9fa626949827b76ae37140ac3&amp;app=powerplay','buttons': [    {'type': 'web_url','url': 'http://www.youtube.com/embed/SQ1W7RsXL3k','title': 'Watch video'    },    {'type': 'web_url','url': 'https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting','title': 'Record'    }]    },    {'title': 'Game of Thrones','subtitle': 'Game of Thrones','image_url': 'http://ia.media-imdb.com/images/M/MV5BMjM5OTQ1MTY5Nl5BMl5BanBnXkFtZTgwMjM3NzMxODE@._V1_UX182_CR0,0,182,268_AL_.jpg','buttons': [    {'type': 'web_url','url': 'https://www.youtube.com/watch?v=36q5NnL3uSM','title': 'Watch video'    },    {'type': 'web_url','url': 'https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting','title': 'Record'    }]    },    {'title': 'The Night Of','subtitle': 'The Night Of','image_url': 'http://ia.media-imdb.com/images/M/MV5BMjQyOTgxMDI0Nl5BMl5BanBnXkFtZTgwOTE4MzczOTE@._V1_UX182_CR0,0,182,268_AL_.jpg','buttons': [    {'type': 'web_url','url': 'https://www.youtube.com/watch?v=36q5NnL3uSM','title': 'Watch video'    },    {'type': 'web_url','url': 'https://m.verizon.com/myverizonmobile/router.aspx?token=tvlisting','title': 'Record'}]}]}}}";
+
+        res.json(yourJson);
+
+    });
+
+
 
 // more routes for our API will happen here
 // REGISTER OUR ROUTES -------------------------------
